97ff522bdf84c2eb3bdaab3380ee845ff91c8aee
PinkiWinkii <asier.arguinchona@ikasle.aeg.eus>
2024-10-11 00:14:51 +0200

    [MAIN][ADD] Add Genre and Rating component

---
+++ components/Genre.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Container = styled.View`
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;    
+`
+
+const GenreContainer = styled.View`
+    border: 1px solid #CCCCCC;
+    border-radius: 3px;
+    margin: 0 2px 2px 0;
+    padding: 3px;
+`
+
+const Text = styled.Text`
+    opacity: 0.5;
+    font-size: 8px;
+`
+interface GenreProps {
+    genres: any;
+}
+
+const Genre: React.FC<GenreProps> = ({genres}) => {
+    return ( 
+        <Container>
+            {genres.map((genre: any, index: any) => {
+                return(
+                    <GenreContainer key={index.toString()}>
+                        <Text>{genre}</Text>
+                    </GenreContainer>
+                )
+            })}
+        </Container>
+    )
+}
+
+export default Genre;
